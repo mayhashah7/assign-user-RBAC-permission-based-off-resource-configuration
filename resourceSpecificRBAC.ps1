1c7c6b609d848c6b13ebb8ff81495eb7956931cd
Mayha Shah <69404742+mayhashah7@users.noreply.github.com>
2020-08-08 20:04:39 -0400

    Add files via upload

---
+++ resourceSpecificRBAC.ps1
@@ -0,0 +1,6 @@
+﻿$windowsVMs = Search-AzGraph -Query "Resources | where type =~ 'Microsoft.Compute/virtualMachines' | where tostring(properties.storageProfile.osDisk.osType) =~ 'Windows'"
+
+foreach ($resource in $windowsVMs) {
+	New-AzRoleAssignment -ObjectId <INSERT USER OBJECT ID HERE> -RoleDefinitionName "Virtual Machine Contributor" -ResourceName $resource.name `
+	-ResourceType $resource.type -ResourceGroupName $resource.resourceGroup
+}
